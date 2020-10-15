5981cb917408f9acf01510f7352c98c1888a98fd
Chloe LANDOLFINI <chlandol@student.42.fr>
2020-10-15 20:23:28 +0200

    ex07 corrected, added space after , and . at end

--- Shell 01/ex07/r_dwssap.sh
+++ Shell 01/ex07/r_dwssap.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cat /etc/passwd | sed '/^#/d' | awk 'NR%2 == 0' | cut -d : -f 1 | rev | sort -rd | sed -n $FT_LINE1,$FT_LINE2\p | tr '\n' ',' | sed 's/\,$/./g' | tr -d "\n"
+cat /etc/passwd | sed '/^#/d' | awk 'NR%2 == 0' | cut -d : -f 1 | rev | sort -rd | sed -n $FT_LINE1,$FT_LINE2\p | tr '\n' ',' | sed 's/,/, /g' | sed 's/\, $/./g' | tr -d "\n"
