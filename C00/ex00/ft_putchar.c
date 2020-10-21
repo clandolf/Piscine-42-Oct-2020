b34e3335c08b6a69dea173b0600fe88209d41f05
Chloe LANDOLFINI <chlandol@student.42.fr>
2020-10-21 19:53:40 +0200

    corrected ex00

--- C00/ex00/ft_putchar.c
+++ C00/ex00/ft_putchar.c
@@ -6,11 +6,11 @@
 /*   By: chlandol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/15 13:21:17 by chlandol          #+#    #+#             */
-/*   Updated: 2020/10/19 17:47:02 by chlandol         ###   ########.fr       */
+/*   Updated: 2020/10/21 19:53:16 by chlandol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <unistd.h>
 
 void	ft_putchar(char c)
 {
